c57c5104d5420f2d50032050a7e454218c6cef26
LinuxBrony <techdude300@gmail.com>
2013-03-13 12:37:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+OSUQDB
+======
+
+Ohio State University Quote Databse.
+
+About
+-----
+This project is based on the [Carnegie Mellon Quote Database](http://cmubash.org/).
+It's a simple Ruby on Rails web app for users to submit quotes, have admins
+approve them, and then be able to search through the quotes.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OSUQDB::Application.config.secret_token = 'e81be6e401e4a0b896fbdd6f863888c833ee5209e31592843f1d57840c9143143fbed5e89ceb8cf8d7dd258f20bb57d260c3334e9ec9b1f4d5ec457cbd7a922f'
